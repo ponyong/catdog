16ec1ea153d5bdf9b832295bf2e2b6d4fef58d7d
ponyong <ponyong@naver.com>
2020-01-14 14:47:31 +0900

    lombok apply

--- src/test/java/com/catdog/springboot/Application.java
+++ src/test/java/com/catdog/springboot/Application.java
@@ -1,13 +1,11 @@
-package com.catdog;
+package com.catdog.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatdogApplication {
-
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(CatdogApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
