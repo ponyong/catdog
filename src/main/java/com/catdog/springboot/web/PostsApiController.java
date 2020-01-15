b57fe425127ced67ad47e29a490a5b520923cfd5
ponyong <ponyong@naver.com>
2020-01-15 11:57:25 +0900

    JPA Update Query Test

---
+++ src/main/java/com/catdog/springboot/web/PostsApiController.java
@@ -0,0 +1,25 @@
+package com.catdog.springboot.web;
+
+
+import com.catdog.springboot.service.PostsService;
+import com.catdog.springboot.web.dto.PostsSaveRequestDto;
+import com.catdog.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+}
